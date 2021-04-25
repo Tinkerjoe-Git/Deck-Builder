c80545ca5efd21df9fa449d74a3f7a22b6f586a2
tinkerjoe-git <nicholas.stagno@gmail.com>
2021-04-25 17:55:18 -0400

    ready for migration

--- db/migrate/20210425204338_create_comments.rb
+++ db/migrate/20210425204338_create_comments.rb
@@ -1,10 +1,10 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-      t.string :content
+      t.text :content
       t.integer :user_id
-      t.resources :user
-      t.resources :deck
+      t.integer :deck_id
+
       t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
