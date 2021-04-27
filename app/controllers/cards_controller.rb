d2f9f273ad2768d6f3e7fb8992d53214b0307ad1
tinkerjoe-git <nicholas.stagno@gmail.com>
2021-04-27 01:36:54 -0400

    added some functionality

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,71 @@
 class CardsController < ApplicationController
+        before_action :redirect_if_not_logged_in, only: [:new, :create, :edit, :update]
+    before_action :find_collage, only: [:index, :new, :create]
+
+    def search
+        @cards = card.search(params[:title])
+        render :index
+    end
+
+    def most_recent
+      
+        @cards = Card.most_recent
+    end
+
+    def index
+        @cards = Card.all
+    end
+    
+    def show
+        @card = Card.find_by(id: params[:id])
+        @comments = @card.comments
+        @comment = @card.comments.build(user_id: current_user.id)
+        @card.update(views: @card.views+1) 
+
+    end
+    
+    def new
+   
+    end
+
+    def create
+        
+        @card = card.new(card_params)
+        if @card.save
+            render :new
+        else
+            flash[:error] = "Could not create card"
+        end
+    end
+
+    def edit
+        @card = card.find_by(id: params[:id] )
+        render :edit
+    end
+
+    def update
+        @card = card.find_by(id: params[:id])
+        @card.update(card_params)
+
+        if @card.valid?
+            redirect_to card_path(@card)
+        else
+            render :edit
+        end
+    end
+
+
+
+    def destroy
+        ## I don't think we want anyone but admin to do this, and even then why.
+        @card = card.find_by(id: params[:id])
+        @card.destroy
+        redirect_to cards_path
+    end
+
+    private
+    #Strong Params
+    def card_params
+        params.require(:card).permit(:name, :description, :card_url, :,)
+    end
 end
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,2 +1,18 @@
-class SessionController < ApplicationController
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    return redirect_to '/login' if !params[:email] || params[:email].empty?
+    user = User.find_by(email: params[:email])
+    user = user.try(:authenticate, params[:password])
+    return redirect_to '/login' unless user
+    session[:user_id] = user.id
+    redirect_to user_path(user)
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to '/login'
+  end
 end
