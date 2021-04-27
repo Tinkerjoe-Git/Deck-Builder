5bf0c5c855ecfe0a81de5c646b18d62a6b242297
tinkerjoe-git <nicholas.stagno@gmail.com>
2021-04-27 16:35:05 -0400

    Prior to change join tables - decklist

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,7 +1,9 @@
 class Deck < ApplicationRecord
   belongs_to :user
   has_many :cards
+  has_many :comments ##reseed?
   has_many :users, through: :comments
+  ####Scope out our breakdown.
 
   validates :name, presence: true
 
