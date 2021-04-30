54ac81bfc96b721cac375ad6b75569e61765c7e1
tinkerjoe-git <nicholas.stagno@gmail.com>
2021-04-30 02:38:48 -0400

    Working on deck creation

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,14 +1,12 @@
 class DecksController < ApplicationController
-    # before_action :find_decklist, only: [:index, :new, :create]
+    # before_action :find_deckcards, only: [:index, :new, :create]
 
     def new
         @deck = Deck.new
-        @decklist = 3.times.collect { @deck.decklists.build }
-        
-        # else
-        #     flash[:notice] = "Couldn't find decklist"
-        # end
+    end
 
+    def index
+        @decks = Deck.all
     end
 
     def edit
@@ -16,12 +14,11 @@ class DecksController < ApplicationController
     end
 
     def create
-        @deck = current_user.decks.new(deck_params)
-        #@deck = @decklist.quantities.build(user_id: current_user.id, contents: current_user.decklist.quantities)
-
+        @deck = Deck.new(deck_params)
+        format_id = Format.find_by(name: params[:name]).id
         if @deck.save
-            @deck.add_decklist_to_deck(decklist_quantities_params)
-            redirect_to deck_path(@deck)
+            @deck.add_cards_to_deck(card_params)
+            redirect_to deck_path(@deck), flash[:notice] = "Your Deck has sucessfully been added"
         else
             render :new
             flash[:notice] = "Decklist couldn't compile, Please try again."
@@ -34,9 +31,6 @@ class DecksController < ApplicationController
 
     def show
         @deck = Deck.find(params[:id])
-        # @decklists = @deck.decklist
-        # @decklist = @deck.decklist.build(user_id: current_user.id, decklist: @decklists.quantities)
-        # @deck.update
     end
 
     def update
@@ -54,11 +48,11 @@ class DecksController < ApplicationController
     private
 
     def deck_params
-        params.require(:deck).permit(:name, :decklist, :user_id, decklists_attributes: [:card_id, :user_id, :quantities])
+        params.require(:deck).permit(:name, :creator, :format_id, :user_id, :total_cards, :mainboard, :sideboard)
     end
 
-    def decklist_quantities_params
-        @decklist = Decklist.find_by_id(params[:decklist_id])
+    def card_params
+        params.require(:card).permit(:name, :text, :power, :toughness, :cmc, :rarity, :card_type, :artist, :colors, :set, :flavor, :mana_cost, :image_url, :loyalty, :number, :multiverse_id, :magic_set_id)
     end
 
 end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -5,7 +5,28 @@ class Deck < ApplicationRecord
   has_many :cards, through: :deck_cards
   validates :name, :format, presence: true
 
+  accepts_nested_attributes_for :deck_cards    
+  accepts_nested_attributes_for :cards
 
   scope :search, -> (query) { self.where("name LIKE ?", "%#{query}%") }
+
+
+  def add_cards_to_deck(params)
+
+    
+    params[:card_attributes].each do |card|
+      new_deck_card = DeckCard.new(
+        deck_id: @deck.id
+        card_id: card[:card_id] || Card.find_by(name: card[:name]).id
+        card_count: card[:count] == '' ? 1 : card[:count],
+        sideboard: card[:sideboard]
+      )
+      new_deck_card.save
+    end
+    @deck.save
+  end
+  
+
+end
 
 
