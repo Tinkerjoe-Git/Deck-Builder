87c33bb4db852b6e1e0c83d35277af7ebc987d4e
tinkerjoe-git <nicholas.stagno@gmail.com>
2021-05-05 16:14:25 -0400

    Initiate getting nested routing

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -16,42 +16,20 @@ class CardsController < ApplicationController
     end
     
     def new
-   
     end
 
     def create
-        
-        @card = card.new(card_params)
-        if @card.save
-            render :new
-        else
-            flash[:error] = "Could not create card"
-        end
     end
 
     def edit
-        @card = card.find_by(id: params[:id] )
-        render :edit
     end
 
     def update
-        @card = card.find_by(id: params[:id])
-        @card.update(card_params)
-
-        if @card.valid?
-            redirect_to card_path(@card)
-        else
-            render :edit
-        end
     end
 
 
 
     def destroy
-        ## I don't think we want anyone but admin to do this, and even then why.
-        @card = card.find_by(id: params[:id])
-        @card.destroy
-        redirect_to cards_path
     end
 
     private
--- notes.md
+++ notes.md
@@ -1,55 +1,6 @@
-## Todo and Structure
+Missing user submittable attribute on the join table " Quantity"
 
-##User
-- has many decks, through: cards
-- has_many :comments
-- has many commented_decks, through :comment
-- username
-- email
-- password_digest
-
-
-
-
-
-##Card
-- belongs_to Deck
-- has image_url
-- text
-- power
-- toughness
-- cmc
-- type
-- colors
-- set
-- mana_cost
-
-
-
-
-
-
-
-##Deck
--belongs_to :user
--has_many card, through :user
--has_many :comments
--has_many user, through :comments
-- decklist
-- sideboard
-- user_id
-
-
-
-
-
-
-##Comment
-- belongs_to :user
-- belongs_to :deck
-- content
-
-
+Missing Nested routes, new* / create and index* or show.
 
 
 
